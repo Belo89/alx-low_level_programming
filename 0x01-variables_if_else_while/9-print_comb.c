7b2ac704cd0721ad35b31dc1c0496389e5906577
Belo89 <aymenbennani200@gmail.com>
2023-10-06 17:55:35 -0700

    1 2 3 4 5 6 7 8 9 100 101 102

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible combinations of single-digit numbers
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
