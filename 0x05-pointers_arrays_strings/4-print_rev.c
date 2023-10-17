f207b070100faa5c7913df1e89af93edffba2604
Belo89 <aymenbennani200@gmail.com>
2023-10-17 05:13:52 -0700

    1 2 3 4 5 6 7 8 9

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+*/
+
+void print_rev(char *s)
+{
+	int count  = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	for (; count > 0; count--)
+	{
+		_putchar(*--s);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: string of characters input
+ *
+ * Return: Nothing
+*/
+
+void puts_half(char *str)
+{
+	int i, count = 0;
+
+	while (str[count])
+		count++;
+
+	if (count % 2 == 0)
+	{
+		for (i = count / 2 ; i < count; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = count / 2 + 1 ; i < count; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+
+}
