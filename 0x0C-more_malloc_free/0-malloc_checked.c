d18559187dda0653367d08d54d38d517b5e6e3b1
Belo89 <aymenbennani200@gmail.com>
2023-11-02 15:57:00 -0700

    0 1 2 3 100 101

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ *
+ * @b: int input
+ *
+ * Return: pointer to the array initialized or NULL
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *s = malloc(b);
+
+	if (!s)
+		exit(98);
+
+	return (s);
+}
